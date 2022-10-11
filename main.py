1d9e20db4762375ecc81780603337849f8b11478
Ian AgustÍn <101183649+IanOzafran@users.noreply.github.com>
2022-10-11 11:27:09 +0000

    a

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('login.html')
-  
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/ayuda')
+def ayuda():
+  return render_template('ayuda.html')
+
+@app.route('/nosotros')
+def nosotros():
+  return render_template('nosotros.html')
+
+@app.route('/politicas')
+def politicas():
+  return render_template('politicas.html')
 
 
 app.run(host='0.0.0.0', port=81)
