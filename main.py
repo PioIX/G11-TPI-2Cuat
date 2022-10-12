3d946832f715c8127bdcf389526b04d24f300bae
Nicolás MAGNO <103133102+nicomagno0@users.noreply.github.com>
2022-10-12 16:08:23 +0000

    a

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template,request,session
 
 app = Flask(__name__)
@@ -22,6 +23,20 @@ def nosotros():
 @app.route('/politicas')
 def politicas():
   return render_template('politicas.html')
+
+@app.route('/loginChef', methods = ['GET', 'POST'])
+def loginChef():
+  if (request.method == 'POST'):
+    if (request.mothod['nombre'] != []):
+      conn = sqlite3.connect('dataBase.db')
+      nombre = request.form['nombre']
+      contra = request.form['password']
+      
+      q = f"""SELECT usuario FROM Chef WHERE nombre = {nombre} and password = '{contra}' ;"""  
+      conn.execute(q)
+      conn.commit()
+      conn.close()
+
 
 
 app.run(host='0.0.0.0', port=81)
