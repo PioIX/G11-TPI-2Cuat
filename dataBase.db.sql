bab45fd17b80169eff17db85f0980a9a42ba457e
Damian Efrain <101183244+dbottini2022pioix@users.noreply.github.com>
2022-10-18 11:15:22 +0000

    e

---
+++ dataBase.db.sql
@@ -0,0 +1,34 @@
+BEGIN TRANSACTION
+CREATE TABLE IF NOT EXISTS'Chefs' (
+	'id_chef'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'usuario'	TEXT NOT NULL,
+	'contra'	TEXT NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS'Recetas' (
+	'id_receta'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'nombre'	TEXT NOT NULL,
+	'descripcion'	TEXT NOT NULL,
+	'pais_origen'	TEXT,
+	'img'	INTEGER,
+	'id_chef'	INTEGER NOT NULL,
+	FOREIGN KEY('id_chef') REFERENCES 'Chefs' ('id_chef')
+  );
+
+CREATE TABLE IF NOT EXISTS'Ingredientes' (
+	'id_ingrediente'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'nombre'	TEXT NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS'Lista_ingedientes' (
+	FOREIGN KEY('id_ingrediente') REFERENCES 'Ingredientes' ('id_ingredientes'),
+  FOREIGN KEY('id_receta') REFERENCES 'Recetas' ('id_receta')
+  );
+
+INSERT INTO 'Chefs' VALUES (1, 'admin', '123G11');
+INSERT INTO 'Ingredientes' VALUES (1, 'aceite');
+INSERT INTO 'Ingredientes' VALUES (2, 'papa');
+INSERT INTO 'Recetas' VALUES (1, 'Papas fritas', 'Fritar las papas en el aceite', 'Argentina', 1, 1);
+INSERT INTO 'Lista_ingredientes' VALUES (1, 1);
+COMMIT;
+
